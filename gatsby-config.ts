62d65096e9768a91d54de5d4cc079a25b76f840d
zx <83443576@qq.com>
2022-06-22 08:11:38 +0800

    Finished Gatsby Tutorial Part 7

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,6 +20,7 @@ const config: GatsbyConfig = {
 			}
 		},
 		"gatsby-plugin-mdx",
+		"gatsby-transformer-sharp"
 	],
 }
 
--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -1,15 +1,21 @@
 import * as React from "react";
 import {graphql} from "gatsby";
 import {MDXRenderer} from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import Layout from "../../components/layout";
+import {IGatsbyImageData} from "gatsby-plugin-image/dist/src/components/gatsby-image.browser";
 
 interface BlogPostProps {
 	data: {
 		mdx: {
 			frontmatter: {
 				title: string,
-				date: string
+				date: string,
+				hero_image_alt: string,
+				hero_image_credit_link: string,
+				hero_image_credit_text: string,
+				hero_image: any
 			},
 			body: string
 		}
@@ -17,9 +23,27 @@ interface BlogPostProps {
 }
 
 const BlogPost = ({data}: BlogPostProps) => {
+	const image= getImage(data.mdx.frontmatter.hero_image)
+
 	return (
 		<Layout pageTitle="Super Cool Blog Posts">
-			<p>{data.mdx.frontmatter.date}</p>
+			<p>Post: {data.mdx.frontmatter.date}</p>
+			{
+				(image !== undefined) && (
+					<>
+						<GatsbyImage
+							image={image}
+							alt={data.mdx.frontmatter.hero_image_alt}
+						/>
+						<p>
+							Photo Credit:{" "}
+							<a href={data.mdx.frontmatter.hero_image_credit_link}>
+								{data.mdx.frontmatter.hero_image_credit_text}
+							</a>
+						</p>
+					</>
+				)
+			}
 			<MDXRenderer>
 				{data.mdx.body}
 			</MDXRenderer>
@@ -28,13 +52,21 @@ const BlogPost = ({data}: BlogPostProps) => {
 }
 
 export const query = graphql`
-	query ($id: String) {
+	query($id: String) {
 		mdx(id: {eq: $id}) {
+			body
 			frontmatter {
 				title
-				date(formatString: "MMMM D, YYYY")
+				date(formatString: "MMMM DD, YYYY")
+				hero_image_alt
+				hero_image_credit_link
+				hero_image_credit_text
+				hero_image {
+					childImageSharp {
+						gatsbyImageData
+					}
+				}
 			}
-			body
 		}
 	}
 `
