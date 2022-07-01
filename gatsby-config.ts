9cd130bbd4bf89a8461c3a315e2107753869d385
zx <83443576@qq.com>
2022-07-01 08:50:37 +0800

    update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
-		title: "My First Gatsby Site",
+		title: "ψ(｀∇´)ψ zx's Site",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
