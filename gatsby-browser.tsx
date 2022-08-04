367bbef06f31e924df32cbca1b00e2fce62aee93
zx <83443576@qq.com>
2022-08-05 07:44:16 +0800

    update style

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -6,57 +6,41 @@ import "terminal.css"
 
 import "./src/styles/typography.css"
 import "./src/styles/main.styl"
+import 'css-doodle'
 
-import Doodle from "./src/components/doodle"
 
-
-const DoodleWrapper = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-`
-
-const ContentWrapper = styled.section`
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  overflow: auto;
-  padding: 5em 0;
-`
-
-const PageWrapper = styled.div`
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-`
-
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'css-doodle': any;
+    }
+  }
+}
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element }) => {
 
   return (
-    <PageWrapper>``
-      <DoodleWrapper>
-        <Doodle rule={`
-          @grid: 1 / 100vw 100vh;
-          background-color: #0a0c27;
-          background-size: 200px 200px;
-          background-image: @doodle(
-            @grid: 6 / 100%;
-            @size: 4px;
-            font-size: 4px;
-            color: hsl(@r240, 30%, 50%);
-            box-shadow: @m3x5(
-              calc(4em - @nx * 1em) calc(@ny * 1em)
-                @p(@m3(currentColor), @m2(#0000)),
-              calc(2em + @nx * 1em) calc(@ny * 1em)
-                @lp
+    <main>
+      <css-doodle class="doodle-background">
+        {`
+            @grid: 1 / 100vw 100vh;
+            background-color: #0a0c27;
+            background-size: 200px 200px;
+            background-image: @doodle(
+              @grid: 6 / 100%;
+              @size: 4px;
+              font-size: 4px;
+              color: hsl(@r240, 30%, 50%);
+              box-shadow: @m3x5(
+                calc(4em - @nx * 1em) calc(@ny * 1em)
+                  @p(@m3(currentColor), @m2(#0000)),
+                calc(2em + @nx * 1em) calc(@ny * 1em)
+                  @lp
+              );
             );
-          );
-        `} />
-      </DoodleWrapper>
-      <ContentWrapper>{element}</ContentWrapper>
-    </PageWrapper>
+          `}
+      </css-doodle>
+      {element}
+    </main>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,12 @@ import styled from "styled-components";
 
 const Section = styled.section`
   background-color: var(--background-color);
-  width: 90vw;
-  margin: 0 auto;
+  width: 100vw;
+  height: 100vh;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const Header = styled.header`
@@ -14,10 +18,15 @@ const Header = styled.header`
 `
 
 const H1 = styled.h1`
-  font-size: 4em;
+  font-size: 7em;
   display: block;
+  text-shadow: -0.1em 0.1em #ff5e5e, 0.05em -0.05em 0em #5e5eff;
 `
 
+const DoodleContainer = styled.div`
+  width: 100%;
+  height: 300px;
+`
 
 const IndexPage = () => {
   return (
