d77e489af544820213a8a5f4e55241fc480b4b08
张鑫 <zhangxin8344@gmail.com>
2022-08-08 18:07:06 +0800

    update

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react"
 import type { GatsbyBrowser } from "gatsby"
 
 import "terminal.css"
-import "./src/styles/typography.css"
+import "./src/styles/typography.min.css"
 import "./src/styles/main.styl"
 
 const BackgroundDoodle = React.lazy(() => import('./src/components/background-doodle'))
